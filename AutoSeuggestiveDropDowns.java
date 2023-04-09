979e7a706daf129802be1d4e5373d6b8c965fcf5
Collins Eban <Collins.Eban@wemabank.com>
2023-04-09 18:31:14 +0100

    Finding dynamic dropdowns using a loop

--- AutoSeuggestiveDropDowns.java
+++ AutoSeuggestiveDropDowns.java
@@ -1,9 +1,53 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class AutoSeuggestiveDropDowns {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver", "/Users/collins.eban/Documents/chromedriver.exe");
+		
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(ops);
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise");
+		//there are drop down that comes based on your input, you input in and it shows india for example
+		
+		driver.findElement(By.id("autosuggest")).sendKeys("India");
+		Thread.sleep(3000);
+		 
+		driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+		//notice above we use find element (s) we are getting more than one item thats why
+		//the return type of find elements is list of element
+		
+		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+		
+		for(WebElement option :options)//anytime loop iterate we pick an option
+		{
+			if(option.getText().equalsIgnoreCase("India"))
+			{
+				option.click();
+				break;
+			}
+		}
+		
+		
+		String[] name = {"Collins", "Abang", "Eban"};
+		for(int z = 0; z < name.length; z++)
+		{
+			System.out.println(name[z]);
+		}
+		
+		//AN ENHANCED LOOP IS AS BELOW
+		for(String s: name)
+		{
+			System.out.println(s);
+		}
 	}
 
 }
--- DynamicDropDown.java
+++ DynamicDropDown.java
@@ -1,9 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DynamicDropDown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-
+		
+System.setProperty("webdriver.chrome.driver", "/Users/collins.eban/Documents/chromedriver.exe");
+		
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(ops);
+		driver.get("http://spicejet.com");
+		//a[@value="MAA"] - xpath for chennai
+		
+		//a[@value='VLR'] - xpath for bal
+		//if two xpath of same value are find you need tell selenium which to click
+		//(//a[@value="MAA"])[2]
+		
+		//first drop down below
+		driver.findElement(By.id("ct100 mainContent ddl originStation CTXT")).click();
+		
+		//second dropdown
+		driver.findElement(By.xpath("a//[@value='BLR']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("a//[@value='MAA']")).click();//gives error because they are 2 and we need the second
+		
+		driver.findElement(By.xpath("(a//[@value='MAA'])[2]")).click();//we use index to specify
+		//but there are people who have issues with Indexes and wont want above, how then will you handle that? look below
+		//YOU CAN ACTUALLY GET A PARENT XPATH, AND THEN A CHILD XPATH LINKED TOGETHER LIKE BELOW
+		//IN THIS CASE IT IS CALLED PARENT CHILD RELATIONSHIP
+		
+		driver.findElement(By.xpath("//div[@id='glsct100_mainContent_ddl_destinationStattion1_CTNR'] //a[@value='MAA']"));
+		//where this: //div[@id='glsct100_mainContent_ddl_destinationStattion1_CTNR'] is the parent and 
+		////a[@value='MAA] is the child
 	}
 
 }
